1f464ebfe076c0292992f811a8757af4268e6f9b
郝雷 <m13522389185@163.com>
2018-12-22 18:02:19 +0800

    pug + koa-views

--- server/index.js
+++ server/index.js
@@ -5,12 +5,26 @@ const logger = require('koa-logger');
 const { normalTpl, ejsTpl, pugTpl } = require('./tpl');
 const ejs = require('ejs')
 const pug = require('pug');
+const views = require('koa-views');
+const { resolve } = require('path');
 
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
+
+// app.use(async (ctx, next) => {
+//     ctx.type = 'text/html;charset=utf-8';
+//     ctx.body = pug.render(pugTpl, {
+//         you: 'pug',
+//         me: 'HaoLLL'
+//     })
+// });
+
+//render经过views的执行 被挂载到ctx上下文中
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html;charset=utf-8';
-    ctx.body = pug.render(pugTpl,{
-        you: 'pug',
+    await ctx.render('index', {
+        you: 'pug with koa-views',
         me: 'HaoLLL'
     })
-});
+})
 app.listen(4455);
